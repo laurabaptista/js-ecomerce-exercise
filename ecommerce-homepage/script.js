58321db32fabb9b9379905f4e6dc67e19b0df096
Laura Baptista <laurabaptista@gmail.com>
2026-08-14 22:29:43 +0100

    feat: fetch products from Fake Store API

--- ecommerce-homepage/script.js
+++ ecommerce-homepage/script.js
@@ -12,3 +12,14 @@ const createProductGridContainer = () => {
 
 document.body.appendChild(createPageTitle());
 document.body.appendChild(createProductGridContainer());
+
+const PRODUCTS_ENDPOINT = "https://fakestoreapi.com/products";
+
+const fetchProducts = async () => {
+  const response = await fetch(PRODUCTS_ENDPOINT);
+  const products = await response.json();
+  console.log(products);
+  return products;
+};
+
+fetchProducts();
